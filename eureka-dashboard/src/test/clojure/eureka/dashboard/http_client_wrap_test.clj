503b10a9a3131e2c27ad5fb97c65634499f008d9
Amit Joshi <ajoshi@netflix.com>
2014-10-11 14:09:52 -0700

    test fix

--- eureka-dashboard/src/test/clojure/eureka/dashboard/http_client_wrap_test.clj
+++ eureka-dashboard/src/test/clojure/eureka/dashboard/http_client_wrap_test.clj
@@ -3,34 +3,44 @@
   (:require [clojure.test :refer :all]
             [eureka.dashboard.http-client-wrap :as wrap]))
 
-(comment (deftest json-parse-good
+(deftest json-parse-good
   []
-  (is (= {:name "amit"} (wrap/json-parse "{\"name\": \"amit\"}")))) (deftest json-parse-bad
-    []
-    (is (= {} (wrap/json-parse "{ame\": \"amit\"}")))) (deftest http-get-simple
-      []
-      (let [c (chan)]
-        (wrap/http-get {:url "http://ip.jsontest.com"
-                        :jsonout false :callback (fn [ip]
-                                                   (is (= (type ip) java.lang.String))
-                                                   (put! c "done"))})
-        (alts!! [c (timeout 2000)]))) (deftest http-get-json
-          []
-          (let [c (chan)]
-            (wrap/http-get {:url "http://ip.jsontest.com"
-                            :jsonout true :callback (fn [ip-resp]
-                                                      (put! c (and
-                                                                (contains? ip-resp :ip)
-                                                                (map? ip-resp))))})
-            (is (first (alts!! [c (timeout 3000)]))))) (deftest http-get-bad-url
-              []
-              (let [c (chan)]
-                (wrap/http-get {:url "http://ip.jsontest.com"
-                                :jsonout false :callback (fn [ip]
-                                                           (put! c (empty? ip)))})
-                (is (first (alts!! [c (timeout 3000)])))
-                )) (comment
-                  (run-tests)))
+  (is (= {:name "amit"} (wrap/json-parse "{\"name\": \"amit\"}"))))
+
+(deftest json-parse-bad
+  []
+  (is (= {} (wrap/json-parse "{ame\": \"amit\"}"))))
+
+(deftest http-get-simple
+  []
+  (let [c (chan)]
+    (wrap/http-get {:url "http://ip.jsontest.com"
+                    :jsonout false :callback (fn [ip]
+                                               (is (= (type ip) java.lang.String))
+                                               (put! c "done"))})
+    (alts!! [c (timeout 2000)])))
+
+(deftest http-get-json
+  []
+  (let [c (chan)]
+    (wrap/http-get {:url "http://ip.jsontest.com"
+                    :jsonout true :callback (fn [ip-resp]
+                                              (put! c (and
+                                                        (contains? ip-resp :ip)
+                                                        (map? ip-resp))))})
+    (is (first (alts!! [c (timeout 3000)])))))
+
+(deftest http-get-bad-url
+  []
+  (let [c (chan)]
+    (wrap/http-get {:url "http://ip.jsontest.com"
+                    :jsonout false :callback (fn [ip]
+                                               (put! c (not-empty ip)))})
+    (is (first (alts!! [c (timeout 3000)])))))
+
+
+(comment
+  (run-tests))
 
 
 
